9c5f0915bfa442462f6ae606f2c9871bead39518
unknown <nazmussadat2120@gmail.com>
2026-09-17 17:22:31 +0600

    Another done today

--- 141A.c
+++ 141A.c
@@ -8,12 +8,27 @@ int main() {
     scanf("%100s", guest);
     scanf("%100s", res);
     scanf("%100s", pile);
-
     int count[26] = {0};
+
     for(int i = 0; guest[i] != '\0'; i++) {
-        count[]
+        count[guest[i] - 'A']++;
     }
 
+    for(int i = 0; res[i] != '\0'; i++) {
+        count[res[i] - 'A']++;
+    }
 
+    for(int i = 0; pile[i] != '\0'; i++) {
+        count[pile[i] - 'A']--;
+    }
+
+    for(int i = 0; i < 26; i++) {
+        if(count[i] != 0) {
+            printf("NO\n");
+            return 0;
+        }
+    }
+
+    printf("YES\n");
     return 0;
 }
