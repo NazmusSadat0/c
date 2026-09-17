5e193e6b32c29cd3289a4443cd5962d367ff2684
unknown <nazmussadat2120@gmail.com>
2026-09-17 16:59:39 +0600

    123

---
+++ 141A.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char guest[101];
+    char res[101];
+    char pile[101];
+    scanf("%100s", guest);
+    scanf("%100s", res);
+    scanf("%100s", pile);
+
+    int count[26] = {0};
+    for(int i = 0; guest[i] != '\0'; i++) {
+        count[]
+    }
+
+
+    return 0;
+}
